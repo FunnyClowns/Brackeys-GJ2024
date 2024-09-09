13c92c46d6fd9ff0541434599371e63bf70170e7
FunnyClowns <faradeaken@gmail.com>
2024-09-09 19:15:10 +0700

    Added animation

--- Assets/Scripts/Framework/Player/PlayerAnimation.cs
+++ Assets/Scripts/Framework/Player/PlayerAnimation.cs
@@ -4,38 +4,80 @@ public class PlayerMovement1 : MonoBehaviour
 {
     [SerializeField] Animator animator;
     PlayerInput playerInput;
-    PlayerMovement playerMovement;
+
+    bool isMovingDown;
+    bool isMovingUp;
+    bool isMovingRight;
+    bool isMovingLeft;
 
     void Start(){
         playerInput = GetComponent<PlayerInput>();
-        playerMovement = GetComponent<PlayerMovement>();
     }
 
     void FixedUpdate(){
 
+        CheckAnimation();
+
+        if (playerInput.MoveValue.x == 0 && playerInput.MoveValue.y == 0){
+            
+            if (isMovingDown){
+                animator.Play("PlayerIdle_Down");
+            }
+
+            if (isMovingUp){
+                animator.Play("PlayerIdle_Up");
+            }
+
+            if (isMovingRight){
+                animator.Play("PlayerIdle_Right");
+            }
+
+            if (isMovingLeft){
+                animator.Play("PlayerIdle_Left");
+            }
+        }
+    }
+
+    void CheckAnimation(){
+
         if (playerInput.MoveValue.y != 0){
             if (playerInput.MoveValue.y > 0){
                 animator.Play("PlayerRun_Up");
+
+                isMovingUp = true;
+                isMovingDown = false;
             }
 
             if (playerInput.MoveValue.y < 0){
                 animator.Play("PlayerRun_Down");
+
+                isMovingDown = true;
+                isMovingUp = false;
             }
+
+            isMovingRight = false;
+            isMovingLeft = false;
+
         } else
         
         if (playerInput.MoveValue.x != 0){
             if (playerInput.MoveValue.x > 0){
                 animator.Play("PlayerRun_Right");
+
+                isMovingRight = true;
+                isMovingLeft = false;
             }
 
             if (playerInput.MoveValue.x < 0){
                 animator.Play("PlayerRun_Left");
+
+                isMovingLeft = true;
+                isMovingRight = false;
             }
 
-        }
+            isMovingDown = false;
+            isMovingUp = false;
 
-        if (playerInput.MoveValue.x == 0 && playerInput.MoveValue.y == 0){
-            animator.Play("Player_Idle");
         }
     }
 }
