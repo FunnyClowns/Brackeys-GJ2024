76f2efdf09a787131484dfcb76eda300fe4b08b4
FunnyClowns <faradeaken@gmail.com>
2024-09-13 23:54:07 +0700

    ingredients text

--- Assets/Scripts/Gameplay/GameMechanic/TakeMeat/FridgeManager.cs
+++ Assets/Scripts/Gameplay/GameMechanic/TakeMeat/FridgeManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using TMPro;
 
 public class FridgeManager : MonoBehaviour {
 
-    [HideInInspector] public int meatAmount = 999;
+    [SerializeField] public int meatAmount = 999;
 
     [SerializeField] PlayerController playerController;
     [SerializeField] Sprite meatSprite;
+
+    [SerializeField] TextMeshProUGUI ingredientsCountText;
+
+    void Start(){
+        ingredientsCountText.text = meatAmount.ToString();
+    }
     
     public void TriggerTakeMeat(){
         DecreaseMeatAmount();
@@ -15,9 +22,16 @@ public class FridgeManager : MonoBehaviour {
         playerController.playerItem.enabled = true;
     }
 
+    void AddMeatAmount(){
+        meatAmount += 1;
+
+        ingredientsCountText.text = meatAmount.ToString();
+    }
+
     void DecreaseMeatAmount(){
         meatAmount -= 1;
 
+        ingredientsCountText.text = meatAmount.ToString();
         Debug.Log("Meat Amount : " + meatAmount);
     }
 
