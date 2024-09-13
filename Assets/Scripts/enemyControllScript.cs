e773ca6ba22febfc076594be19f1fdd6727f41fd
SNIPER-61385 <realsniperapps@gmail.com>
2024-09-13 19:13:52 -0400

    Start of enemy controll

---
+++ Assets/Scripts/enemyControllScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyControllScript : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
