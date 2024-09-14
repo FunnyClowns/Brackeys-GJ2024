aa3122f182e3d7cc5ea25fdb89573e5e298616a4
FunnyClowns <faradeaken@gmail.com>
2024-09-14 21:07:11 +0700

    Makes clients rush move

--- Assets/Scripts/Gameplay/GameMechanic/RushHour/RushHourController.cs
+++ Assets/Scripts/Gameplay/GameMechanic/RushHour/RushHourController.cs
@@ -6,7 +6,8 @@ public class RushHourController : MonoBehaviour
 {
     
     [SerializeField] GameManager gameManager;
-    [SerializeField] PlayableDirector clientsTransition;
+    [SerializeField] PlayableDirector clientsRushTransition;
+    [SerializeField] PlayableDirector clientsCalmTransition;
 
     Vector2 RushHourTime;
     Vector2 RushHourTimeCountdown;
@@ -28,18 +29,21 @@ public class RushHourController : MonoBehaviour
 
         yield return new WaitForSeconds(randomCountdownTime);
 
-        clientsTransition.Play();
+        clientsRushTransition.Play();
 
         yield return new WaitForSeconds(7f);
 
+        clientsRushTransition.Stop();
         gameManager.StartRushHour();
 
         yield return new WaitForSeconds(randomTime);
 
+        clientsCalmTransition.Play();
+
+        yield return new WaitForSeconds(7f);
+
+        clientsCalmTransition.Stop();
+
         StartCoroutine(RushHourCountdown());
-    }
-
-    void MoveMonster(){
-
     }
 }
