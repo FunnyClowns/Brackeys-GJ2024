ede736f7e4b9f6cdcc8ae62793fefb474c12f027
FunnyClowns <faradeaken@gmail.com>
2024-09-22 22:49:23 +0700

    base ui fighting mechanic

--- Assets/Scripts/Gameplay/GameMechanic/Fighting/Enemies/EnemyFightingController.cs
+++ Assets/Scripts/Gameplay/GameMechanic/Fighting/Enemies/EnemyFightingController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class healthScript : MonoBehaviour
+public class EnemyFightingController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
